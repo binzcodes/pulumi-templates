09612c6a125149d3cc064d9cbeccec4976b0fd74
Binzi <r@rbi.nz>
2022-06-13 20:32:52 +0000

    feat: elinting

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 This repo contains a template for pulumi new, which makes it easy to quickly get started building new Pulumi projects.
 
-```
+```bash
 pulumi new https://github.com/binzcodes/pulumi-template-aws
 ```
 
@@ -22,25 +22,26 @@ Start a minimal serverless API template using AWS API Gateway and Lambda functio
 # run
 
 To use this template install [Pulumi cli](https://www.pulumi.com/docs/get-started/install/) and run:
-```
+```bash
 pulumi new https://github.com/binzcodes/pulumi-template-aws
 ```
 
 Deploy resources:
 
-```
+```bash
 pulumi up
 ```
 
 Test the endpoint:
 
-```
+```bash
+pulumi stack select && \
 for i in {1..5}; do curl $(pulumi stack output url); done
 ```
 
 Clean up resources:
 
-```
+```bash
 pulumi destroy
 pulumi stack rm
 ```
@@ -51,7 +52,6 @@ Contrbutions welcome
 [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/binzcodes/pulumi-minimal-dynamodb-api)
 
 # Todo
-- Linting with Prettier
 - Testing with jest
 - CI/CD with Github Actions
 
--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import * as AWS from "aws-sdk";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as AWS from 'aws-sdk';
+import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
 
-const hits = new aws.dynamodb.Table("hits", {
-  attributes: [{name: "Site", type: "S"}],
-  hashKey: "Site",
-  billingMode: "PAY_PER_REQUEST",
+const hits = new aws.dynamodb.Table('hits', {
+  attributes: [{ name: 'Site', type: 'S' }],
+  hashKey: 'Site',
+  billingMode: 'PAY_PER_REQUEST',
 });
 
 const logHitsEventHandler = async () => {
@@ -13,27 +13,27 @@ const logHitsEventHandler = async () => {
   const result = await dc
     .update({
       TableName: hits.name.get(),
-      Key: {Site: "ACMECorp"},
-      UpdateExpression: "SET Hits = if_not_exists(Hits, :zero) + :incr",
-      ExpressionAttributeValues: {":zero": 0, ":incr": 1},
-      ReturnValues: "UPDATED_NEW",
+      Key: { Site: 'ACMECorp' },
+      UpdateExpression: 'SET Hits = if_not_exists(Hits, :zero) + :incr',
+      ExpressionAttributeValues: { ':zero': 0, ':incr': 1 },
+      ReturnValues: 'UPDATED_NEW',
     })
     .promise();
   return {
     statusCode: 200,
-    headers: {"Content-Type": "text/json"},
+    headers: { 'Content-Type': 'text/json' },
     body: JSON.stringify({
-      messgae: "Welcome to ACMECorp!",
+      messgae: 'Welcome to ACMECorp!',
       hits: result.Attributes!.Hits,
     }),
   };
 };
 
-const site = new awsx.apigateway.API("site", {
+const site = new awsx.apigateway.API('site', {
   routes: [
     {
-      path: "/",
-      method: "GET",
+      path: '/',
+      method: 'GET',
       eventHandler: logHitsEventHandler,
     },
   ],
